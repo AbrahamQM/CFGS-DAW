86f023705fdf15bae39a6c6c7ddab6f5fcb2a492
Abraham Quintana <abrahamqm87@gmail.com>
2024-11-03 17:03:28 +0000

    End of script Task 10

--- Desarrollo Web en Entorno Cliente DEW/UT6 - Modelo de objetos del documento en javascript/Tarea 10 - Modelo de objetos del documento en javascript/js/scriptTarea10.js
+++ Desarrollo Web en Entorno Cliente DEW/UT6 - Modelo de objetos del documento en javascript/Tarea 10 - Modelo de objetos del documento en javascript/js/scriptTarea10.js
@@ -33,14 +33,13 @@ const codigo = () =>{
    const mostrarIslas = (e) => {
        const provincia = e.target.value;
        const gruposDeIslas = document.getElementsByTagName("optgroup");
-        console.log("mostrarIslas:" , provincia);
         if (provincia !== " "){
             // por si el usuario cambia de provincia después de seleccionar una isla
             ocultarListas();//oculto todas las listas de pueblos de nuevo
             islas.selectedIndex = 0; // reinicio el select de islas
             
             labels[1].style.visibility = "visible"; //muestro la siguiente etiqueta y el select
-            document.getElementById("islas").style.visibility = "visible"; 
+            islas.style.visibility = "visible"; 
             
             for(const grupo of gruposDeIslas){ //selecciono las islas que corresponden
                 if(grupo.label === provincia){
@@ -64,39 +63,53 @@ const codigo = () =>{
     es decir, se detecte un cambio con el evento change, hacemos visible los 3 pueblos de esa Isla y los 
     posicionamos al lado del select en lugar de verlos en el lugar que ocuparía dentro del html. A cada uno de 
     esos 3 pueblos debemos añadirle un addEventListener()
-
     */
+    const contenedor = document.createElement("div");
+    let pueblos;
+    let isla;
     const seleccionarIsla = (e) =>{
-        const isla = e.target.value;
-        const pueblos = document.getElementById(isla);
+        isla = e.target.value;
+        pueblos = document.getElementById(isla);//obtengo el listado de pueblos que corresponde.
         ocultarListas();//oculto todas las listas de pueblos por si el usuario cambia de isla
         if (isla !== " "){
-            labels[2].style.visibility = "visible"; //hago visible el contenedor_img
-            const rutaImagen = `/islas/${isla}.png`; 
-            const imagen = document.createElement("img"); //creo la imagen y le seteo los atributos
-            imagen.src = rutaImagen;
-            imagen.alt = `Imagen ${isla}`;
-            imagen.style = "width: 100px; height: 100px;"
-            labels[2].appendChild(imagen); //añado al contenedor la imagen
-            labels[2].appendChild(pueblos); //añado al contenedor el listado
-            labels[2].style.display = "flex-line";
-            pueblos.style.visibility = "visible"; //TODO quitar
-            pueblos.style.display = "block"; // Cambiado a display: block para mostrar el listado
-            pueblos.style.position = "inline"; // Aseguro que el <ul> no afecte a su posición en el documento
-            // labels[1].appendChild(pueblos);
-            // pueblos.style.display = "block";
-            // labels[1].classList.add('inline-block');
+            const hijos = labels[2].children;
+            for (const hijo of hijos) { //por si se cambia de isla elimino los posibles hijos que tenga
+                labels[2].removeChild(hijo);
+            }
+            labels[2].appendChild(contenedor);
+            contenedor.appendChild(pueblos);
+            contenedor.style.visibility = "visible";
+            contenedor.appendChild(pueblos);
+            // contenedor.style.position = "absolute"; 
+            contenedor.style.width = "30%";
+            contenedor.style.display = "inline-block";
+            pueblos.style.visibility = "visible"; 
+            for (const p of pueblos.children) { //añado los lístener
+                p.addEventListener("click", elegirPueblo, false);
+            }
         }else{
             ocultarListas();
         }
     }
     document.getElementById("islas").addEventListener("change", seleccionarIsla, false);
-
-/*
+    
+    /*
     d) Función elegir pueblo: Al pinchar encima del texto de uno de los pueblos anteriormente mostrados, 
     debemos cambiar el color del texto a rojo y de los no seleccionados a azul. Deberá mostrar la imagen de la Isla, 
     llamamos a la siguiente función.
+    */
+   let pueblo;
+   const elegirPueblo = (e) => {
+       pueblo = e.target;
+       for (const p of pueblos.children){
+           p.style.color = "blue";
+        };
+        pueblo.style.color = "red";
+        mostrarImagen();//Llamo al método que muestra la imagen
+    }
 
+
+/*
     e) Función mostrar imagen isla: Para ello debemos crear un elemento nuevo document.createElement de una imagen, 
     le fijaremos los atributos src y alt con setAttribute y la añadiremos al contenedor id="contenedor_imag" que se 
     encuentra en el fichero.html. Le daremos un tamaño adecuado a la imagen y su posición. Y mostraremos un texto que 
@@ -105,5 +118,27 @@ const codigo = () =>{
     haciendo uso de document.innerHTML. Cambiaremos su posición desde JavaScript para que en lugar de ocupar la que 
     ocupa en el HTML esté debajo de la imagen y la lista de los 3 pueblos, y su color a verde.
 */
+    const mostrarImagen = () => {
+        const rutaImagen = `/islas/${isla}.png`; 
+        const imagen = document.createElement("img"); //creo la imagen y le seteo los atributos
+        const contenedorImg = document.getElementById("contenedor_imag");
+        contenedorImg.style.display = "inline-block";
+        contenedorImg.style.visibility = "visible"; 
+        pueblos.style.visibility = "visible"; 
+        imagen.src = rutaImagen;
+        imagen.alt = `Imagen ${isla}`;
+        imagen.style = "width: 100px; height: 100px;";
+        contenedorImg.appendChild(pueblos);
+        contenedorImg.appendChild(imagen); //añado al contenedor la imagen
+
+        //añado el texto al parrafo
+        const parrafo = document.getElementById("parrafo");
+        contenedorImg.appendChild(parrafo);
+        parrafo.style.color = "green";
+        parrafo.innerText = `Has elegido ${pueblo.innerText} es un municipio de la isla de ${islas.options[islas.selectedIndex].text}`;
+    }
+
+    // NOTA para Yaiza: No he conseguido colocar correctamente 
+
 }
 window.onload = codigo;
