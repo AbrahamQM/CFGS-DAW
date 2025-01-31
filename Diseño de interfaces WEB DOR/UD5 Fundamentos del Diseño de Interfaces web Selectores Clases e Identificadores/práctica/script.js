00b731efa74a11187a1a37061c099b5a6871912e
Abraham Quintana <abrahamqm87@gmail.com>
2025-01-31 22:22:30 +0000

    improved task 'design a table UD5'

--- Diseño de interfaces WEB DOR/UD5 Fundamentos del Diseño de Interfaces web Selectores Clases e Identificadores/práctica/script.js
+++ Diseño de interfaces WEB DOR/UD5 Fundamentos del Diseño de Interfaces web Selectores Clases e Identificadores/práctica/script.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.createElement('button');
-    button.textContent = 'Saludo Navideño';
+    const texto = 'No es el diseño mas bonito, peeeero....,\nTiene un poquito de todo, bootstrap, js, degradados, selectores de todo tipo, ...\nEspero que este te guste más'
+    button.textContent = 'Despliega el mensaje';
+    button.className = "btn btn-outline-warning";
+    button.style.backgroundColor = '#A1D071';
     button.addEventListener('click', function() {
-        alert('¡Feliz Navidad!');
+        alert(texto);
     });
-    document.body.appendChild(button);
+    const divContainer = document.querySelector('.container');
+    divContainer.appendChild(button);
 });
