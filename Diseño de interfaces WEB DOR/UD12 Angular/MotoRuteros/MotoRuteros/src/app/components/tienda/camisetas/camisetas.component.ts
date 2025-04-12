2c8b5b2ced63c8c3789118657c508e4366739780
Abraham Quintana <abrahamqm87@gmail.com>
2025-04-12 12:30:20 +0100

    #ending-project: done CamisetasComponent

--- Diseño de interfaces WEB DOR/UD12 Angular/MotoRuteros/MotoRuteros/src/app/components/tienda/camisetas/camisetas.component.ts
+++ Diseño de interfaces WEB DOR/UD12 Angular/MotoRuteros/MotoRuteros/src/app/components/tienda/camisetas/camisetas.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-camisetas',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './camisetas.component.html',
   styleUrl: './camisetas.component.css'
 })
