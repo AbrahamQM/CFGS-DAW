35f29c8417f41f7bd35057aa9c44a694ee5c230b
Abraham Quintana <abrahamqm87@gmail.com>
2025-04-12 18:14:24 +0100

    #ending-project: Done GaleriaComponent

--- Diseño de interfaces WEB DOR/UD12 Angular/MotoRuteros/MotoRuteros/src/app/components/galeria/galeria.component.ts
+++ Diseño de interfaces WEB DOR/UD12 Angular/MotoRuteros/MotoRuteros/src/app/components/galeria/galeria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // Cargar la API de YouTube
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag);
 
+    // Esperar a que la API esté lista
+    (window as any).onYouTubeIframeAPIReady = () => {
+      const iframes = document.querySelectorAll('.video-frame');
+      iframes.forEach((iframe: any) => {
+        const player = new (window as any).YT.Player(iframe, {
+          events: {
+            onReady: (event: any) => {
+              iframe.parentElement.addEventListener('mouseover', () => event.target.playVideo());
+              iframe.parentElement.addEventListener('mouseout', () => event.target.pauseVideo());
+            }
+          }
+        });
+      });
+    };
+  }
 }
